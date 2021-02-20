8ba92da44770682488501933770a6a66f0b61dc3
Alexander Saltykov <temp.kroogi@gmail.com>
2021-02-20 20:12:27 +0300

    Updates

---
+++ strategy-demo-scene/precache-manifest.7b91b6dedea346b1990b14b512d519c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "a4a976688306e6de9c70fde0a29c10a8",
+    url: "/2ba05b3336fcb33dda17.worker.js",
+  },
+  {
+    revision: "18f1972d090b2172ddfd8ff23c002e10",
+    url: "/5b4d96d23a5943112a7b.worker.js",
+  },
+  {
+    revision: "c03e34808b1c126215089a4f18116eec",
+    url: "/index.html",
+  },
+  {
+    revision: "fbf7b6a39f5bcf0551b2",
+    url: "/static/css/main.0cd11301.chunk.css",
+  },
+  {
+    revision: "a7fa1828d9112246537c",
+    url: "/static/js/2.194900ae.chunk.js",
+  },
+  {
+    revision: "496957ac5aa25aadf65e3f3fb50aec00",
+    url: "/static/js/2.194900ae.chunk.js.LICENSE.txt",
+  },
+  {
+    revision: "fbf7b6a39f5bcf0551b2",
+    url: "/static/js/main.98d15060.chunk.js",
+  },
+  {
+    revision: "024c8a4f0b3e5e675766",
+    url: "/static/js/runtime-main.d6902202.js",
+  },
+])
